9c3fcdac65e3cf56f87b09c5a12768ca040d5631
Aballo Janet <jabalo@student.refactory.academy>
2024-08-05 10:22:55 +0300

    updated watchlist

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,27 +1,60 @@
-import React, { useState, useEffect } from "react"; //this enables us to acess all react functionalities that we may need
+import React, { useState } from "react"; //this enables us to acess all react functionalities that we may need
 import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
+export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
   const [searchTerm, setSearchTerm] = useState("");
 
   const [genre, setGenre] = useState("All Genres");
   const [rating, setRating] = useState("All");
 
-
-  useEffect(() => {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
-
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  const filteredMovies = movies.filter(movie=>
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
+  const matchesGenre = (movie, genre) => {
+    return (
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
+    );
+  };
+
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All":
+        return true;
+
+      case "Good":
+        return movie.rating >= 8;
+
+      case "Ok":
+        return movie.rating >= 5 && movie.rating < 8;
+
+      case "Bad":
+        return movie.rating < 5;
+
+      default:
+        return false;
+    }
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      matchesGenre(movie, genre) &&
+      matchesRating(movie, rating) &&
+      matchesSearchTerm(movie, searchTerm)
   );
 
   return (
@@ -36,32 +69,44 @@ export default function MoviesGrid() {
 
       <div className="filter-bar">
         <div className="filter-slot">
-            <label>Genre</label>
-            <select className="filter-dropdown">
-                <option>All Genres</option>
-                <option>Action</option>
-                <option>Drama</option>
-                <option>Fantasy</option>
-                <option>Horror</option>
-            </select>
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
         </div>
         <div className="filter-slot">
-            <label>Rating</label>
-            <select className="filter-dropdown">
-                <option>All</option>
-                <option>Good</option>
-                <option>Ok</option>
-                <option>Bad</option>
-            </select>
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
         </div>
       </div>
 
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id}></MovieCard>
+          <MovieCard
+            movie={movie}
+            key={movie.id}
+            toggleWatchlist={toggleWatchlist}
+            isWatchlisted={watchlist.includes(movie.id)}
+          ></MovieCard>
         ))}
       </div>
-
     </div>
   );
 }
